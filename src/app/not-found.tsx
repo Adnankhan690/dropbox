9de8a1a2453e90927945bbd483ca7d46ccae03df
adnan <adnan@aventistechnology.com>
2025-05-27 23:20:22 +0530

    refactor the code

--- src/app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound () {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-4">404</h1>
-      <p className="text-xl">Page not found</p>
-    </div>
-  );
-}
